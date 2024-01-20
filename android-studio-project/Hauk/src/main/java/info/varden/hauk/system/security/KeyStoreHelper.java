e6b804ef70b34ef2e3fc947ef08f2b56385c24c2
Warren R. Bank <warren.r.bank@gmail.com>
2024-01-19 17:53:28 -0800

    fix KeyStoreHelper

--- android-studio-project/Hauk/src/main/java/info/varden/hauk/system/security/KeyStoreHelper.java
+++ android-studio-project/Hauk/src/main/java/info/varden/hauk/system/security/KeyStoreHelper.java
@@ -6,15 +6,19 @@ import android.security.KeyPairGeneratorSpec;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.AlgorithmParameterSpec;
+import java.util.Calendar;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.GCMParameterSpec;
+import javax.security.auth.x500.X500Principal;
 
 import info.varden.hauk.utils.Log;
 
@@ -28,12 +32,11 @@ public final class KeyStoreHelper {
     @SuppressWarnings("HardCodedStringLiteral")
     private static final String ANDROID_KEY_STORE = "AndroidKeyStore";
     @SuppressWarnings("HardCodedStringLiteral")
-    private static final String TRANSFORMATION = "AES/GCM/NoPadding";
-    private static final int GCM_AUTH_TAG_LENGTH = 128;
+    private static final String TRANSFORMATION = "RSA/ECB/PKCS1Padding";
 
     private static Context context = null;
     private static KeyStore store = null;
-    private SecretKey key = null;
+    private KeyPair keyPair = null;
 
     public static void initStaticResources(Context context) {
         KeyStoreHelper.context = context;
@@ -51,17 +54,23 @@ public final class KeyStoreHelper {
 
             // Check if the alias exists. If not, create it.
             if (!store.containsAlias(alias.getAlias())) {
-                Log.i("Generating new key for alias %s", alias); //NON-NLS
+                Log.i("Generating new keypair for alias %s", alias); //NON-NLS
 
-                KeyGenerator keygen = KeyGenerator.getInstance("AES", ANDROID_KEY_STORE);
+                KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", ANDROID_KEY_STORE);
                 AlgorithmParameterSpec spec = this.getAlgorithmParameterSpec(alias);
-                keygen.init(spec);
-                this.key = keygen.generateKey();
+                generator.initialize(spec);
+                this.keyPair = generator.generateKeyPair();
             }
             else {
-                Log.i("Loading existing key for alias %s", alias); //NON-NLS
-                KeyStore.SecretKeyEntry keyEntry = (KeyStore.SecretKeyEntry) store.getEntry(alias.getAlias(), null);
-                this.key = keyEntry.getSecretKey();
+                Log.i("Loading existing keypair for alias %s", alias); //NON-NLS
+
+                PrivateKey privateKey = (PrivateKey) store.getKey(alias.getAlias(), null);
+                PublicKey  publicKey  = (PublicKey)  store.getCertificate(alias.getAlias()).getPublicKey();
+
+                if (privateKey == null || publicKey == null)
+                    throw new Exception("Unable to load keypair for alias");
+
+                this.keyPair = new KeyPair(publicKey, privateKey);
             }
         }
         catch (Exception e) {
@@ -74,16 +83,22 @@ public final class KeyStoreHelper {
 
         if (Build.VERSION.SDK_INT >= 23) {
             spec = (AlgorithmParameterSpec) new KeyGenParameterSpec.Builder(alias.getAlias(), KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .setBlockModes(KeyProperties.BLOCK_MODE_ECB)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
                 .build();
         }
         else if (Build.VERSION.SDK_INT >= 18) {
             if (KeyStoreHelper.context != null) {
+                Calendar startDate = Calendar.getInstance();
+                Calendar endDate   = Calendar.getInstance(); endDate.add(Calendar.YEAR, 20);
+
                 spec = (AlgorithmParameterSpec) new KeyPairGeneratorSpec.Builder(KeyStoreHelper.context)
-                    .setAlias(alias.getAlias())
-                    .setKeyType("AES")
-                    .build();
+                  .setAlias(alias.getAlias())
+                  .setSerialNumber(BigInteger.ONE)
+                  .setSubject(new X500Principal("CN=" + alias.getAlias() + " CA Certificate"))
+                  .setStartDate(startDate.getTime())
+                  .setEndDate(endDate.getTime())
+                  .build();
             }
             else {
                 throw new Exception("Context is null");
@@ -107,11 +122,11 @@ public final class KeyStoreHelper {
         Log.v("Encrypting data"); //NON-NLS
 
         // Catch errors during initialization.
-        if (this.key == null) throw new EncryptionException(new InvalidKeyException("Encryption key is null"));
+        if (this.keyPair == null) throw new EncryptionException(new InvalidKeyException("Encryption keypair is null"));
 
         try {
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            cipher.init(Cipher.ENCRYPT_MODE, this.key);
+            cipher.init(Cipher.ENCRYPT_MODE, this.keyPair.getPublic());
             byte[] iv = cipher.getIV();
             byte[] message = cipher.doFinal(data);
             return new EncryptedData(iv, message);
@@ -131,12 +146,11 @@ public final class KeyStoreHelper {
         Log.v("Decrypting data"); //NON-NLS
 
         // Catch errors during initialization.
-        if (this.key == null) throw new EncryptionException(new InvalidKeyException("Decryption key is null"));
+        if (this.keyPair == null) throw new EncryptionException(new InvalidKeyException("Decryption keypair is null"));
 
         try {
             Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-            GCMParameterSpec spec = new GCMParameterSpec(GCM_AUTH_TAG_LENGTH, data.getIV());
-            cipher.init(Cipher.DECRYPT_MODE, this.key, spec);
+            cipher.init(Cipher.DECRYPT_MODE, this.keyPair.getPrivate());
             return cipher.doFinal(data.getMessage());
         } catch (Exception e) {
             throw new EncryptionException(e);
